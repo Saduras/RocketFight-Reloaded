445fca94f0639a3b697f47498aa2f68a24bc6298
David Speck <david@saduras.de>
2015-11-22 12:01:37 +0100

    Setup base movement and looking input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+    Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        var movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        var lookInput = new Vector3(Input.GetAxis("RX"), 0f, Input.GetAxis("RY"));
+
+        _rigidbody.AddForce(movementInput * speed, ForceMode.Force);
+        transform.LookAt(transform.position + lookInput * 100f);
+	}
+}
