155057e1629714986f8271c084b7def39e36cf3d
David Speck <david@saduras.de>
2015-11-22 12:34:27 +0100

    Setup base projectile rocket

---
+++ Assets/Scripts/RocketMotor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RocketMotor : MonoBehaviour {
+
+    public Rigidbody rocketRigidbody;
+    public float speed = 10f;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        rocketRigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+	}
+}
