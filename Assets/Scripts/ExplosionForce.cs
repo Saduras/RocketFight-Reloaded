556a6fbc40989ad8c13e72c6fadd819ac93c32f9
David Speck <david@saduras.de>
2016-02-06 12:49:23 +0100

    Explosions!

---
+++ Assets/Scripts/ExplosionForce.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class ExplosionForce : MonoBehaviour {
+
+    public float force = 10f;
+    public ForceMode forceMode = ForceMode.Force;
+    public float selfDestructionTime = 2f;
+
+    float _radius;
+    List<Collider> _hitObjects;
+    float _liveTime;
+
+	void Start () {
+        _radius = (GetComponent<Collider>() as SphereCollider).radius;
+        _liveTime = 0f;
+        _hitObjects = new List<Collider>();
+    }
+
+    void Update() {
+        _liveTime += Time.deltaTime;
+
+        if (_liveTime > selfDestructionTime)
+            Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        // Don't hit the same object twice
+        if (_hitObjects.Contains(other))
+            return;
+
+        _hitObjects.Add(other);
+
+        // Apply force to hit object
+        var rigidbody = other.GetComponent<Rigidbody>();
+        if(rigidbody != null) {
+            var directedForce = (rigidbody.transform.position - this.transform.position).normalized * force;
+            rigidbody.AddForce(directedForce, forceMode);
+        }
+    }
+}
---
+++ Assets/Scripts/ExplosionTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class ExplosionTrigger : MonoBehaviour {
+
+    public GameObject explosionPrefab;
+    public GameObject meshParent;
+    public float destructionDelay = 1f;
+
+    bool _triggered;
+    float _destructionTime;
+
+    void Start()
+    {
+        _triggered = false;
+        _destructionTime = float.MaxValue;
+    }
+
+    void Update()
+    {
+        if(_destructionTime < Time.time) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (_triggered)
+            return;
+
+        _triggered = true;
+        _destructionTime = Time.time + destructionDelay;
+        meshParent.SetActive(false);
+
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+    }
+}
