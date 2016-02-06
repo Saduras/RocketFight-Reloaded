9da3822bfdabdcf7e8d8591bd2ae1365657360e3
David Speck <david@saduras.de>
2016-02-06 12:48:15 +0100

    Add start velocity to rockets\

--- Assets/Scripts/RocketMotor.cs
+++ Assets/Scripts/RocketMotor.cs
@@ -3,10 +3,27 @@
 public class RocketMotor : MonoBehaviour {
 
     public Rigidbody rocketRigidbody;
-    public float speed = 10f;
-	
-	// Update is called once per frame
-	void LateUpdate () {
-        rocketRigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+    public float startVelocity = 15f;
+    public float acceleration = 10f;
+    public float selfDestructionTime = 5f;
+
+    float _liveTime = 0f;
+
+    void Start()
+    {
+        rocketRigidbody.velocity = transform.forward * startVelocity;
+    }
+
+    void Update()
+    {
+        _liveTime += Time.deltaTime;
+
+        if (_liveTime > selfDestructionTime)
+            Destroy(this.gameObject);
+    }
+
+    void LateUpdate () {
+        if(rocketRigidbody)
+            rocketRigidbody.AddForce(transform.forward * acceleration, ForceMode.Acceleration);
 	}
 }
