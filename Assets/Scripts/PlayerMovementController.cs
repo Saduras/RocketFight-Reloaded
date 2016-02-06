baf518fef631161213e182ea9c56a52baa85457a
David Speck <david@saduras.de>
2016-02-06 12:48:58 +0100

    Apply physics velocity to player movement & add control loss factor

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -8,6 +8,7 @@ public class PlayerMovementController : MonoBehaviour {
 
     public Player player = Player.P1;
     public float speed = 10f;
+    public float controlLossVelocity = 2f;
 
     Rigidbody _rigidbody;
 
@@ -19,7 +20,13 @@ public class PlayerMovementController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         var movementInput = new Vector3(Input.GetAxis(player.ToString() + HORIZONTAL_AXIS), 0f, Input.GetAxis(player.ToString() + VERTICAL_AXIS));
+        var controlledMovement = movementInput * speed;
 
-        _rigidbody.MovePosition(_rigidbody.position + movementInput * speed * Time.deltaTime);
+        var controlFactor = 1 - Mathf.Clamp(_rigidbody.velocity.magnitude, 0f, controlLossVelocity) / controlLossVelocity;
+        var totalMovement = controlledMovement * controlFactor + _rigidbody.velocity;
+
+        _rigidbody.MovePosition(_rigidbody.position + totalMovement * Time.deltaTime);
+
+        Debug.DrawRay(this.transform.position, _rigidbody.velocity, Color.red);
 	}
 }
